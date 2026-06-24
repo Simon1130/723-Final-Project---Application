5ac584a1470417f56352e7249adb0639285f3d14
Simon1130 <3075302l@student.gla.ac.uk>
2026-06-24 12:13:52 +0100

    Main menu frame without function

--- Apache Airlines Booking System.py
+++ Apache Airlines Booking System.py
@@ -22,4 +22,33 @@ def make_floor_plan():
         floor_plan[row][77] = "S"
     
     return floor_plan
-print(make_floor_plan())
+floor_plan = make_floor_plan()
+
+#make main menu
+while True:
+    print("---Apache Airline Seat Booking System---")
+    print("1. Check availability of seat")
+    print("2. Book a seat")
+    print("3. Free a seat")
+    print("4. Show booking status")
+    print("5. Exit program")
+    
+    choice = input("Please select an option from 1 to 5: ")
+    
+    if choice == "1":
+        print("\n[Checking availability of seat.]\n")
+        
+    elif choice == "2":
+        print("\n[Booking a seat.]\n")
+        
+    elif choice == "3":
+        print("\n[Freeing a seat.]\n")
+        
+    elif choice == "4":
+        print("\n[Showing booking status.]\n")
+        
+    elif choice == "5":
+        print("\n[Exiting program. Thank You for choosing Apache Airlines.]\n")
+        break
+    else:
+        print("\n[Invaild choice. Please input a number (1 - 5).]\n")
