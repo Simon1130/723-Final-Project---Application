85b1bda6c1364e3726ee2028dfe01b8df01b96b7
Simon1130 <3075302l@student.gla.ac.uk>
2026-06-26 17:45:43 +0100

    option 1

--- Apache Airlines Booking System.py
+++ Apache Airlines Booking System.py
@@ -23,8 +23,45 @@ def make_floor_plan():
     
     return floor_plan
 floor_plan = make_floor_plan()
-
 booked_seats = [] #global variable
+#translor row letter into row index number
+row_translator = {"A": 0, "B": 1, "C":2, "D":4, "E": 5, "F": 6} 
+
+def option_1(floor_plan):
+    
+    seat_input = input("Please enter a seat to check: ").upper()
+    
+    column_string = seat_input[:-1]
+    row_letter = seat_input[-1]
+    
+    #check row is between A to F and column is number
+    if not column_string.isdigit() or row_letter not in row_translator:
+        print("Invalid input. Rows is A-F and column is number of 1-80.(e.g. 1A)\n")
+        return
+    
+    #check column number is in range of 1 to 80 or not
+    col_num = int(column_string)
+    
+    if col_num < 1 or col_num > 80:
+        print("Invalid column number. Column should be between 1 to 80.\n")
+        return
+    
+    #convert row letter and column into python style
+    row = row_translator[row_letter]
+    column = col_num - 1
+    
+    book_or_not = floor_plan[row][column]
+    
+    if book_or_not == "F":
+        print(f"{seat_input} is available to book.")
+    elif book_or_not == "R":
+        print(f"{seat_input} is booked.")
+    elif book_or_not == "S":
+        print(f"{seat_input} is a storage area.")
+    elif book_or_not == "X":
+        print(f"{seat_input} is an isles.")
+
+
 
 def option_4(floor_plan):
     #summary of booked seats
